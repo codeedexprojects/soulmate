f326f835c2e0a1833b0ff2c67c0fbe1c496a438e
codeedexprojects <codeedexprojects@gmail.com>
2025-03-18 13:24:24 +0530

    host

--- soulmate/settings.py
+++ soulmate/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x5&idpfvbu^9dt7$_a*nm9q*3ns7052gt0z+#rgpvm170wcp#n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

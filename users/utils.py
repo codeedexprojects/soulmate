212096902ddae4c0bad048de96f62cd673feccd2
codeedexprojects <codeedexprojects@gmail.com>
2025-04-05 14:39:51 +0530

    otpp

--- users/utils.py
+++ users/utils.py
@@ -6,7 +6,9 @@ from agora_token_builder import RtcTokenBuilder
 
 def send_otp_2factor(mobile_number, otp):
     api_key = settings.TWO_FACTOR_API_KEY
-    url = f"https://2factor.in/API/V1/{api_key}/SMS/{mobile_number}/{otp}"
+    template_name = "Koottu_app"  # This is your approved template name
+    url = f"https://2factor.in/API/V1/{api_key}/SMS/{mobile_number}/AUTOGEN/{template_name}"
+
 
     response = requests.get(url)
     data = response.json()

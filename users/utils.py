6d0498ff56f590a443a764ce4cb15e90e4c3ef39
codeedexprojects <codeedexprojects@gmail.com>
2025-04-05 16:23:20 +0530

    mkmk

--- users/utils.py
+++ users/utils.py
@@ -4,15 +4,17 @@ import time
 import random
 from agora_token_builder import RtcTokenBuilder
 
-def send_otp_2factor(mobile_number, otp):
+def send_otp_2factor(mobile_number):
+
     api_key = settings.TWO_FACTOR_API_KEY
-    url = f"https://2factor.in/API/V1/<api_key>/SMS/<mobile_number>/<otp>/Voicy"
+    template_name = "Voicy"
+    url = f"https://2factor.in/API/V1/{api_key}/SMS/{mobile_number}/AUTOGEN/{template_name}"
 
     response = requests.get(url)
     data = response.json()
 
     if data.get('Status') == 'Success':
-        return True
+        return data.get('Details')  # session_id to be used for OTP verification
     else:
         raise Exception(f"Failed to send OTP: {data.get('Details')}")
 

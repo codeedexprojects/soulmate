6374496c35e2e53bb9223f9546b7f5489271a656
codeedexprojects <codeedexprojects@gmail.com>
2025-04-05 16:19:59 +0530

    otppp

--- users/utils.py
+++ users/utils.py
@@ -6,7 +6,7 @@ from agora_token_builder import RtcTokenBuilder
 
 def send_otp_2factor(mobile_number, otp):
     api_key = settings.TWO_FACTOR_API_KEY
-    url = f"https://2factor.in/API/V1/{api_key}/SMS/{mobile_number}/{otp}"
+    url = f"https://2factor.in/API/V1/<api_key>/SMS/<mobile_number>/<otp>/Voicy"
 
     response = requests.get(url)
     data = response.json()

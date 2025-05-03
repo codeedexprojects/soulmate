b8922cb7b0a816435cd0a512ad992ec4e2710a4f
codeedexprojects <codeedexprojects@gmail.com>
2025-05-03 16:43:50 +0530

    middleware remove

--- executives/middleware.py
+++ executives/middleware.py
@@ -1,36 +1,36 @@
-from django.utils import timezone
-from django.contrib.sessions.models import Session
-from .models import Executives
-from django.http import HttpResponseRedirect 
+# from django.utils import timezone
+# from django.contrib.sessions.models import Session
+# from .models import Executives
+# from django.http import HttpResponseRedirect 
 
-class AutoLogoutMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
+# class AutoLogoutMiddleware:
+#     def __init__(self, get_response):
+#         self.get_response = get_response
 
-    def __call__(self, request):
-        if not hasattr(request, 'session'):
-            return self.get_response(request)
+#     def __call__(self, request):
+#         if not hasattr(request, 'session'):
+#             return self.get_response(request)
 
-        executive_id = request.session.get('executive_id')
-        if executive_id:
-            try:
-                executive = Executives.objects.get(id=executive_id)
+#         executive_id = request.session.get('executive_id')
+#         if executive_id:
+#             try:
+#                 executive = Executives.objects.get(id=executive_id)
                 
-                if executive.check_activity_timeout():
-                    executive.online = False
-                    executive.is_logged_out = True
-                    executive.current_session_key = None
-                    executive.save()
+#                 if executive.check_activity_timeout():
+#                     executive.online = False
+#                     executive.is_logged_out = True
+#                     executive.current_session_key = None
+#                     executive.save()
                     
-                    if request.session.session_key:
-                        Session.objects.filter(session_key=request.session.session_key).delete()
-                    request.session.flush()
-                    return HttpResponseRedirect('/login?error=session_expired')  # Redirect to login
+#                     if request.session.session_key:
+#                         Session.objects.filter(session_key=request.session.session_key).delete()
+#                     request.session.flush()
+#                     return HttpResponseRedirect('/login?error=session_expired')  # Redirect to login
                 
-                executive.last_activity = timezone.now()
-                executive.save()
+#                 executive.last_activity = timezone.now()
+#                 executive.save()
             
-            except Executives.DoesNotExist:
-                pass  
+#             except Executives.DoesNotExist:
+#                 pass  
 
-        return self.get_response(request)
+#         return self.get_response(request)

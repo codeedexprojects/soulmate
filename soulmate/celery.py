ccfb0b2a642704a531d8f87c4a6da15f58fbdb2d
codeedexprojects <codeedexprojects@gmail.com>
2025-05-03 16:42:53 +0530

    mkmkmkmkmk

--- soulmate/celery.py
+++ soulmate/celery.py
@@ -1,14 +1,1 @@
-# # soulmate/celery.py
-# import os
-# from celery import Celery
-
-# # Set the default Django settings module
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soulmate.settings')
-
-# app = Celery('soulmate')
-
-# # Configure Celery using settings from Django settings.py
-# app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# # Load task modules from all registered Django apps
-# app.autodiscover_tasks()
+# 

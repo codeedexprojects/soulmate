22e6eeef5666198c1304a4ec5732d5ed4aeeba50
Prethewram <prithewramprithew1@gmail.com>
2025-06-25 16:14:15 +0530

    coins2

--- analytics/views.py
+++ analytics/views.py
@@ -99,10 +99,10 @@ class PlatformAnalyticsView(APIView):
         purchases_week = purchases_all.filter(purchase_date__gte=start_of_week)
         purchases_month = purchases_all.filter(purchase_date__gte=start_of_month)
 
-        revenue_today = purchases_today.aggregate(total=Sum('recharge_plan.base_price'))['total'] or 0
-        revenue_week = purchases_week.aggregate(total=Sum('purchased_price'))['total'] or 0
-        revenue_month = purchases_month.aggregate(total=Sum('purchased_price'))['total'] or 0
-        revenue_all = purchases_all.aggregate(total=Sum('purchased_price'))['total'] or 0
+        revenue_today = purchases_today.aggregate(total=Sum('recharge_plan__base_price'))['total'] or 0
+        revenue_week = purchases_week.aggregate(total=Sum('recharge_plan__base_price'))['total'] or 0
+        revenue_month = purchases_month.aggregate(total=Sum('recharge_plan__base_price'))['total'] or 0
+        revenue_all = purchases_all.aggregate(total=Sum('recharge_plan__base_price'))['total'] or 0
 
         coins_today = purchases_today.aggregate(total=Sum('coins_purchased'))['total'] or 0
         coins_week = purchases_week.aggregate(total=Sum('coins_purchased'))['total'] or 0

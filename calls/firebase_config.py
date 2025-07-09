cf8f662d9ba72f7e4d0435470b96e4b4ff675329
Prethewram <prithewramprithew1@gmail.com>
2025-07-09 19:10:12 +0530

    fcm7

--- calls/firebase_config.py
+++ calls/firebase_config.py
@@ -1,6 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("voicyuser-firebase-adminsdk-fbsvc-64693b6329.json")  
+cred = credentials.Certificate("soulmate/voicyuser-firebase-adminsdk-fbsvc-64693b6329.json")
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)

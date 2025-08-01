0ea7485e83169448409eeb2375b13f8b2d269df8
Prethewram <prithewramprithew1@gmail.com>
2025-08-01 09:55:34 +0530

    purchase2

--- analytics/views.py
+++ analytics/views.py
@@ -108,7 +108,11 @@ class PlatformAnalyticsView(APIView):
         revenue_today = purchases_today.aggregate(total=Sum('recharge_plan__base_price'))['total'] or 0
         revenue_week = purchases_week.aggregate(total=Sum('recharge_plan__base_price'))['total'] or 0
         revenue_month = purchases_month.aggregate(total=Sum('recharge_plan__base_price'))['total'] or 0
-        revenue_all = purchases_all.aggregate(total=Sum('recharge_plan__base_price'))['total'] or 0
+        revenue_all = (
+            purchases_all
+            .filter(is_admin=False)
+            .aggregate(total=Sum('recharge_plan__base_price'))['total'] or 0
+        )
 
         coins_today = purchases_today.aggregate(total=Sum('coins_purchased'))['total'] or 0
         coins_week = purchases_week.aggregate(total=Sum('coins_purchased'))['total'] or 0

ccbaa9ea50a073ec77ca9edf23d5715803046955
Sahal Sabith <sahalsabith111@gmail.com>
2025-09-14 23:44:09 +0530

    sfd

--- calls/routing.py
+++ calls/routing.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .consumers import CallConsumer, CallsConsumer
+from .consumers import CallsConsumer
 
 websocket_urlpatterns = [
-    path('ws/calls/', CallConsumer.as_asgi()),
     path('ws/calls-management/', CallsConsumer.as_asgi()),  # New calls management consumer
 ]
